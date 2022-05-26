f639996804e1b6367cc95b963425740d698dcc20
fotih yusupov <fotihyusupov04@gmail.com>
2022-05-26 17:38:53 +0500

    js

--- js/main.js
+++ js/main.js
@@ -7,16 +7,15 @@ const list = document.querySelector('.list-group')
 
 function AddItem(e) {
     e.preventDefault()
-    const item = document.createElement('li')
-    item.className = 'list-group-item p-3 mt-4 mb-4 me-4 ms-4'
-    item.textContent = 'Name:' + username.value.trim() +' '+'Order:'+ userOrder.value.trim() +' '+'Email:'+ userEmail.value
-    const checkBox = document.createElement('input')
-    checkBox.className = 'd-inline-block me-auto'
-    checkBox.type = 'checkbox'
-    item.prepend(checkBox, )
-    list.append(item ,)
     if (username.value.trim() && userOrder.value.trim()){
-        
+        const item = document.createElement('li')
+        item.className = 'list-group-item p-3 mt-4 mb-4 me-4 ms-4'
+        item.textContent = 'Name:' + username.value.trim() +' '+'Order:'+ userOrder.value.trim() +' '+'Email:'+ userEmail.value
+        const checkBox = document.createElement('input')
+        checkBox.className = 'd-inline-block me-auto'
+        checkBox.type = 'checkbox'
+        item.prepend(checkBox, )
+        list.append(item ,)
     } else {
         alert("iltimos ism familya va buyurmangiz nomini qaytadan kirgazing")
     }
